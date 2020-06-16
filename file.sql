f001b6119c774ece8fd3a33e296283472e907809
vkvamsikonijeti <vkvamsikonijeti@gmail.com>
2020-06-16 16:41:06 +0530

    Update file.sql

--- file.sql
+++ file.sql
@@ -1,4 +1,4 @@
-alter schema if exists schema1 rename to schema420;
+alter schema if exists schema1 rename to schema421;
 ALTER TABLE IF EXISTS FRB_EMP_DATA ADD COLUMN SALARY VARCHAR(100);
 select * from FRB_EMP_DATA;
 
